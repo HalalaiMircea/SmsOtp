b421c62b0c7cce9cc60d1c80893d439022b1b669
Halalai Mircea <boss.mircea67@yahoo.com>
2020-07-27 10:33:51 +0300

    Update .gitignore and added php api scripts

---
+++ webapi/htdocs/insert3.php
@@ -0,0 +1,22 @@
+<?php
+require '../htdocsConnectDB/ConnectDB.php';
+
+//require 'ConnectDB.php';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "INSERT INTO `Users` (`UserID`, `Username`) VALUES (NULL, 'test')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+?>
