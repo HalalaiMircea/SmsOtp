230950f08446894321ab295602502ed0eb27a4ab
Halalai Mircea <boss.mircea67@yahoo.com>
2020-09-21 18:59:15 +0300

    Improved permission request handling

--- app/src/main/java/com/example/smsotp/ui/StatusFragment.java
+++ app/src/main/java/com/example/smsotp/ui/StatusFragment.java
@@ -3,9 +3,9 @@ package com.example.smsotp.ui;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.Build;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Process;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,43 +13,48 @@ import android.view.ViewGroup;
 import android.widget.CompoundButton;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.smsotp.BuildConfig;
+import com.example.smsotp.R;
 import com.example.smsotp.WebService;
 import com.example.smsotp.databinding.FragmentStatusBinding;
 import com.example.smsotp.viewmodel.StatusViewModel;
+import com.google.android.material.snackbar.Snackbar;
 
 import static android.Manifest.permission.SEND_SMS;
+import static androidx.core.content.ContextCompat.checkSelfPermission;
 
 public class StatusFragment extends Fragment {
     private static final String TAG = "SMSOTP_StatusFragment";
+    private static final int PERMISSION_REQUEST = 10;
+    private static final int APP_SETTINGS_REQUEST = 11;
     private FragmentStatusBinding binding;
     private StatusViewModel viewModel;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
+        binding = FragmentStatusBinding.inflate(inflater, container, false);
         viewModel = new ViewModelProvider(this).get(StatusViewModel.class);
+        return binding.getRoot();
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
-        binding = FragmentStatusBinding.inflate(inflater, container, false);
-
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         viewModel.getServerState().observe(getViewLifecycleOwner(), binding.serverSwitch::setChecked);
         viewModel.getIpLiveData().observe(getViewLifecycleOwner(), binding.ipTextView::setText);
         binding.dbTextView.setText(viewModel.getDatabaseName());
         binding.portTextView.setText(viewModel.getServerPort());
 
         binding.serverSwitch.setOnCheckedChangeListener(this::onCheckedChanged);
-        if (BuildConfig.DEBUG) {
+
+        if (checkSelfPermission(requireContext(), SEND_SMS) == PackageManager.PERMISSION_DENIED) {
+            requestPermissions(new String[]{SEND_SMS}, PERMISSION_REQUEST);
+        } else if (BuildConfig.DEBUG) {
             Log.d(TAG, "Server started automatically in debug build variant!");
             binding.serverSwitch.setChecked(true);
         }
-        return binding.getRoot();
     }
 
     @Override
@@ -59,35 +64,47 @@ public class StatusFragment extends Fragment {
     }
 
     private void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        Log.i(TAG, "Switch state changed to " + (isChecked ? "on" : "off"));
         Context context = requireContext();
-        Intent serviceIntent = new Intent(context, WebService.class);
+        Intent intent = new Intent(context, WebService.class);
         if (isChecked) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
-                    context.checkSelfPermission(SEND_SMS) == PackageManager.PERMISSION_DENIED) {
-                requestPermissions(new String[]{SEND_SMS}, 10);
-            } else {// If sdk < M or permission is granted
-                context.startService(serviceIntent);
-            }
+            context.startService(intent);
         } else {
-            context.stopService(serviceIntent);
+            context.stopService(intent);
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
-        if (requestCode == 10) {
+        if (requestCode == PERMISSION_REQUEST) {
             // If request is cancelled, the result arrays are empty.
-            if (grantResults.length == 0)
-                return;
-            if (permissions[0].equals(SEND_SMS) && grantResults[0] == PackageManager.PERMISSION_DENIED) {
-                Log.e(TAG, "Permission " + permissions[0] + " denied by user!");
-                Process.killProcess(Process.myPid());
-            } else {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Log.i(TAG, "Permission " + permissions[0] + " granted by user!");
-                requireContext().startService(new Intent(getContext(), WebService.class));
+            } else {
+                Log.e(TAG, "Permission " + permissions[0] + " denied by user!");
+                binding.serverSwitch.setEnabled(false);
+                showSnackbarRationale();
             }
         }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == APP_SETTINGS_REQUEST) {
+            if (checkSelfPermission(requireContext(), SEND_SMS) == PackageManager.PERMISSION_GRANTED)
+                binding.serverSwitch.setEnabled(true);
+            else
+                requestPermissions(new String[]{SEND_SMS}, PERMISSION_REQUEST);
+        }
+    }
+
+    private void showSnackbarRationale() {
+        Snackbar.make(requireView(), R.string.sms_permission_rationale, Snackbar.LENGTH_INDEFINITE)
+                .setAction(R.string.action_settings, v -> {
+                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                            Uri.parse("package:" + requireContext().getPackageName()));
+                    startActivityForResult(intent, APP_SETTINGS_REQUEST);
+                })
+                .show();
     }
 }
